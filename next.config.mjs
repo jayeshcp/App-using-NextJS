0928932c55ca70e1fd700b9b4ea0a280dfe5dce3
Jayesh Chandrapal <jaycp.iitkgp@gmail.com>
2024-10-12 00:46:30 +0000

    Update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/app-using-nextjs/",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
